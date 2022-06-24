cc6e309eaf8dbc2e04470efc3539eb001946b6e4
Hernán Astudillo <hernan.astudillo.r@gmail.com>
2022-06-24 17:22:33 -0400

    fix() add missing requiremints

--- drinks/views.py
+++ drinks/views.py
@@ -7,8 +7,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-from drinks import serializers
-
 
 @api_view(['GET', 'POST'])
 def drink_list(request, format=None):
